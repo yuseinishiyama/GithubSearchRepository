a052e7430166b1f23c4147c6dd7276e28eef9eb3
Yusei Nishiyama <yusei-nishiyama@cookpad.com>
2015-10-02 00:30:30 +0900

    Add commented out HTTPMethods

--- GithubSearchRepository/HTTPMethod.swift
+++ GithubSearchRepository/HTTPMethod.swift
@@ -10,4 +10,12 @@ import Foundation
 
 enum HTTPMethod : String {
     case GET
+//    case POST
+//    case PUT
+//    case HEAD
+//    case DELETE
+//    case PATCH
+//    case TRACE
+//    case OPTIONS
+//    case CONNECT
 }
